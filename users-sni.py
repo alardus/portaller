b77bf8a538797cebeec163184df87d2a9eda522a
Alexander Bykov <alardus@alardus.org>
2014-07-15 10:29:24 +0400

    - added SNI users counting - logging enabled in sni config - some fixes in script for counting users of dns

---
+++ users-sni.py
@@ -0,0 +1,22 @@
+#!/bin/python
+count = 0
+data = [] 
+proc = []
+
+with open('sniproxy.log', 'r') as fl:
+	fl = fl.readlines()
+
+	for i in fl:
+		if 'portaller.com' not in i:
+			i = i.split()
+			i = i[2].split(':')
+			data.append(i[0])
+		else:
+			pass
+
+for i in data:
+	if i not in proc:
+		proc.append(i)
+		count = count + 1
+
+print count
