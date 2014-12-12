0ac734ccb50b83993befd0e02e1b0727f3919986
Alexander Bykov <alardus@alardus.org>
2014-12-12 06:39:27 +0300

    fix connection counter location

--- connections.sh
+++ connections.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/bin/netstat -atW | grep EST | awk {'print $5'} | cut -d : -f 1 | sort | uniq | grep -v pandora | grep -v spotify | grep -v amazon | grep -v netflix | grep -v rdio | grep -v portaller | wc -l > ./connections.txt
+/bin/netstat -atW | grep EST | awk {'print $5'} | cut -d : -f 1 | sort | uniq | grep -v pandora | grep -v spotify | grep -v amazon | grep -v netflix | grep -v rdio | grep -v portaller | wc -l > /var/www/portaller/connections.txt
