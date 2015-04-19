758af69955d0a6d91400d3ff9da1b31a4521dab9
Alexander Bykov <alardus@alardus.org>
2015-04-19 13:11:16 +0300

    spoofed geoCheck for Hulu

--- portaller.py
+++ portaller.py
@@ -26,6 +26,10 @@ def index():
 def index():
 	return template('vpn', dict(error = None, year = copyright))
 
+@app.route('/geoCheck')
+def index():
+        return template('geoCheck.xml', dict(error = None, year = copyright))
+
 @app.route('/status')
 def index():
 	try:
