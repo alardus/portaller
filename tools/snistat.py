86fb58e4ff2d670ba9ec070d0106f80fd9f0045f
Alexander Bykov <alardus@alardus.org>
2015-05-08 12:23:56 +0300

    show portaller stats for yesterday

--- tools/snistat.py
+++ tools/snistat.py
@@ -1,6 +1,25 @@
-import os
+import getopt, sys, os
+
 date = os.popen("date +%Y-%m-%d").read().strip()
-#date = '2015-03-31'
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hy")
+    except getopt.GetoptError as err:
+        print(err)
+        sys.exit(2)
+    for o, a in opts:
+        if o in ("-h"):
+            print 'Try -y argument'
+        elif o in ("-y"):
+        	global date
+        	date = os.popen('date --date yesterday +%Y-%m-%d').read().strip()
+        else:
+            assert False, "unhandled option"
+
+if __name__ == "__main__":
+    main()
+
 with open('/tmp/sniproxy.log', 'r'):
 	p = os.popen("cat /tmp/sniproxy.log | grep " + date + "| grep pandora | awk '{print $3}' | cut -d : -f 1 | uniq | wc -l").read()
 	s = os.popen("cat /tmp/sniproxy.log | grep " + date + "| grep spotify | awk '{print $3}' | cut -d : -f 1 | uniq | wc -l").read()
